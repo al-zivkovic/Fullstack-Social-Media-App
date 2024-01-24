b0a49d5912d67fd4fefc5d5553fa9ad29d463d12
al-zivkovic <91557608+al-zivkovic@users.noreply.github.com>
2024-01-24 15:12:26 -0800

    Create Post functionality
    Created the functionality for creating posts and they can be stored and retrieved from the MongoDB.

--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -1,16 +1,24 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+
 import Post from './Post/post';
 import useStyles from './styles';
+import { CircularProgress, Grid } from '@material-ui/core';
 
 const Posts = () => {
+    const posts = useSelector((state) => state.posts);
     const classes = useStyles();
 
     return (
-        <>
-            <h1>POSTS</h1>
-            <Post />
-            <Post />
-        </>
+        !posts.length ? <CircularProgress /> : (
+            <Grid className={classes.container} container alignItems='stretch' spacing={3}>
+                {posts.map((post) => (
+                    <Grid key={post._id} item xs={12} sm={6}>
+                        <Post post={post} />
+                    </Grid>
+                ))}
+            </Grid>
+        )
     );
 }
 
