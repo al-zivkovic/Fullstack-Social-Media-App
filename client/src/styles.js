4b9a6cdc4eb586fd489f72ecc7c9d811cb9eb612
al-zivkovic <91557608+al-zivkovic@users.noreply.github.com>
2024-01-24 15:20:04 -0800

    Changed the backgound
    Just a nice touch to the background and created it more visually appealing using a free SVG background

--- client/src/styles.js
+++ client/src/styles.js
@@ -10,7 +10,7 @@ export default makeStyles(() => ({
     alignItems: 'center',
   },
   heading: {
-    color: 'rgba(0,183,255, 1)',
+    color: 'rgba(159,90,253, 1)',
   },
   image: {
     marginLeft: '15px',
