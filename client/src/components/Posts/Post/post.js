423236ddfcfd90919710fe602a3cddf24c53485a
al-zivkovic <91557608+al-zivkovic@users.noreply.github.com>
2024-01-24 17:40:23 -0800

    Like and delete post functionality
    Added the functionality to like and delete posts that have been created and posted onto the web app.

--- client/src/components/Posts/Post/post.js
+++ client/src/components/Posts/Post/post.js
@@ -5,9 +5,13 @@ import DeleteIcon from '@material-ui/icons/Delete';
 import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
 import moment from 'moment';
 import useStyles from './styles';
+import { useDispatch } from 'react-redux';
+
+import { deletePost, likePost } from '../../../actions/posts';
 
 const Post = ({ post, setCurrentId }) => {
     const classes = useStyles();
+    const dispatch = useDispatch();
     
     return (
         <Card className={classes.card}>
@@ -32,11 +36,11 @@ const Post = ({ post, setCurrentId }) => {
                 <Typography variant='h5' gutterBottom>{post.message}</Typography>
             </CardContent>
             <CardActions className={classes.cardActions}>
-                <Button size='small' color='primary' onClick={() => {}}>
+                <Button size='small' color='primary' onClick={() => dispatch(likePost(post._id))}>
                     <ThumbUpAltIcon fontSize='small' />
                     Like {post.likeCount}
                 </Button>
-                <Button size='small' color='primary' onClick={() => {}}>
+                <Button size='small' color='primary' onClick={() => dispatch(deletePost(post._id))}>
                     <DeleteIcon fontSize='small' />
                     Delete
                 </Button>
