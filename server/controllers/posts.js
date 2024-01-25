c7256c997942673618561b563ddb817f33accfd3
al-zivkovic <91557608+al-zivkovic@users.noreply.github.com>
2024-01-24 17:10:52 -0800

    Edit posts functionality
    Added the functionality to edit posts that have already been posted to the web app.

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,4 +1,5 @@
 import PostMessage from "../models/postMessage.js";
+import mongoose from 'mongoose';
 
 export const getPosts =  async (req, res) => {
     try {
@@ -23,4 +24,17 @@ export const createPost = async (req, res) => {
         res.status(409).json({ message: error.message})
 
     }
+}
+
+export const updatePost = async (req, res) => {
+    const { id } = req.params;
+    const { title, message, creator, selectedFile, tags } = req.body;
+    
+    if (!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send(`No post with id: ${id}`);
+
+    const updatedPost = { creator, title, message, tags, selectedFile, _id: id };
+
+    await PostMessage.findByIdAndUpdate(id, updatedPost, { new: true });
+
+    res.json(updatedPost);
 }
